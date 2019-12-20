94cb52005d8fa30869a69faa7d854c8abe5bf5c8
karanhinduja <karan_hinduja@hotmail.com>
2019-12-20 08:35:23 +0530

    added functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -40,17 +40,17 @@ app.get('/warm', async (req, res) => {
   const empRef = db.collection('CompanyMaster').doc('LTI').collection('EmpMaster').where('Department', '==', 'DevOps');
   var data: any = [];
   await empRef.get()
-  .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-          // doc.data() is never undefined for query doc snapshots
-          data.push({docID:doc.id, Emp:doc.data()});
-          console.log(doc.id, " => ", doc.data());
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        data.push({ docID: doc.id, Emp: doc.data() });
+        console.log(doc.id, " => ", doc.data());
 
-        });
-  })
-  .catch(function(error) {
+      });
+    })
+    .catch(function (error) {
       console.log("Error getting documents: ", error);
-  });
+    });
   res.send(data);
 
 })
@@ -59,41 +59,192 @@ app.get('/warmTo', (req, res) => {
   res.send('Calentando para la pelea');
 })
 
+
+app.post('/Login', async (request, response) => {
+
+  try {
+    const document = db.collection('CompanyMaster/' + request.body.CompanyCode + '/EmpMaster').doc('/' + request.body.PSNo + '/');
+    let emp = await document.get();
+    let empData = emp.data();
+    if (empData && empData.Password === request.body.Password) {
+      return response.status(200).send({ Success: true, Message: 'success' });
+    } else {
+      return response.status(200).send({ Success: false, Message: 'failed' });
+    }
+
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send(error);
+  }
+});
+
 app.post('/Register', async (request, response) => {
 
   try {
-    const { CompanyCode, Address, EmpMaster } = request.body;
-    const data = {
-      CompanyCode,
-      Address,
-      EmpMaster
+    const query = db.collection('CompanyMaster');
+    var isExist = false;
+    await query.get().then(querySnapshot => {
+      let docs = querySnapshot.docs;
+      for (let doc of docs) {
+        if (doc.id === request.body.CompanyCode) {
+          isExist = true;
+          break;
+        }
+      }
+      return null;
+    }).catch((err) => { console.error(err); });
+
+    const document = db.collection('CompanyMaster').doc('/' + request.body.CompanyCode + '/');
+    var userData = request.body.EmpMaster;
+    userData.SecretSanta = '';
+    if (isExist) {
+      userData.IsAdmin = false;
+    } else {
+      userData.IsAdmin = true;
+      await document.create({
+        CompanyCode: request.body.CompanyCode,
+        Address: request.body.Address
+      });
     }
-
-    //check here if company does not exsist
-    EmpMaster['IsAdmin'] = true;
-
-    const companyRef = await db.collection('CompanyMaster').doc(data.CompanyCode).set({
-      CompanyCode,
-      Address
-    });
-    companyRef.writeTime;
-
-    await db.collection('CompanyMaster').doc(data.CompanyCode)
-      .collection('DepartmentMaster').add({ Name: EmpMaster.Department });
-
-    const empRef = await db.collection('CompanyMaster').doc(data.CompanyCode)
-      .collection('EmpMaster').doc(EmpMaster['PSNo']).set(EmpMaster);
-
-    empRef.writeTime;
-
-    response.json({ success: 'true', message: 'You Have successfully registered' });
+    document.collection('EmpMaster').doc('/' + userData.PSNo + '/').create(userData);
+    document.collection('DepartmentMaster').doc('/' + userData.Department + '/').create({ name: userData.Department });
+    return response.status(200).send({ Success: true, Message: 'registered' });
 
   } catch (error) {
-
-    response.status(500).send(error);
-
+    return response.status(500).send(error);
   }
 });
+
+//get dept
+app.post('/GetDepartmentList', async (request, response) => {
+
+  try {
+    let responseData: any = [];
+    await db.collection('CompanyMaster').doc(request.body.CompanyCode).get()
+      .then(async doc => {
+        if (doc.exists) {
+          await db.collection('CompanyMaster/' + request.body.CompanyCode + '/DepartmentMaster').get().
+            then(sub => {
+              if (sub.docs.length > 0) {
+                for (let doc of sub.docs) {
+                  const selectedItem = {
+                    Name: doc.id,
+                  };
+                  responseData.push(selectedItem);
+                }
+              }
+            });
+        }
+      }).catch((err) => { console.error(err); });
+    return response.status(200).send({ Success: true, Message: 'Department List', data: responseData });
+  } catch (error) {
+    return response.status(500).send(error);
+  }
+});
+
+//get Company
+app.get('/GetCompanyList', async (req, res) => {
+  try {
+    let query = await db.collection('CompanyMaster');
+    let responseData: any = [];
+    await query.get().then(querySnapshot => {
+      let docs = querySnapshot.docs;
+      //console.log(docs);
+      for (let doc of docs) {
+        const selectedItem = {
+          CompanyCode: doc.id,
+        };
+        responseData.push(selectedItem);
+      }
+      // return responseData;
+    }).catch((err) => { console.error(err); });
+    return res.status(200).send({ Success: true, Message: 'Company List', data: responseData });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+app.post('/GetEmpWishlist', async (req, res) => {
+
+  try {
+    const document = db.collection('CompanyMaster/' + req.body.CompanyCode + '/EmpMaster/' + req.body.PSNo + '/wishlist').doc('wishlist');
+    let wish = await document.get();
+    let wishList = wish.data();
+    if (wishList) {
+      return res.status(200).send({ Success: true, Message: 'found', data: wishList });
+    }
+    return res.status(200).send({ Success: false, Message: 'not found', data: {} });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+app.post('/UpdateEmpWishList', async (req, res) => {
+  try {
+    console.log(req.body.WishList);
+    const document = db.collection('CompanyMaster/' + req.body.CompanyCode + '/EmpMaster/' + req.body.PSNo + '/wishlist').doc('wishlist');
+    await document.create(req.body.WishList);
+    return res.status(200).send({ Success: true, Message: 'wishlist created' });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+app.post('/GetEmpProfile', async (req, res) => {
+  try {
+    let document = db.collection('CompanyMaster/' + req.body.CompanyCode + '/EmpMaster').doc('/' + req.body.PSNo + '/');
+    let profile = await document.get();
+    let profileData = profile.data();
+    if (profileData) {
+      delete profileData.Password;
+      return res.status(200).send({ Success: true, Message: 'found', data: profileData});
+    } else {
+      return res.status(200).send({ Success: false, Message: 'not found', data: {} });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+
+// app.post('/Register', async (request, response) => {
+
+//   try {
+//     const { CompanyCode, Address, EmpMaster } = request.body;
+//     const data = {
+//       CompanyCode,
+//       Address,
+//       EmpMaster
+//     }
+
+//     //check here if company does not exsist
+//     EmpMaster['IsAdmin'] = true;
+
+//     const companyRef = await db.collection('CompanyMaster').doc(data.CompanyCode).set({
+//       CompanyCode,
+//       Address
+//     });
+//     companyRef.writeTime;
+
+//     await db.collection('CompanyMaster').doc(data.CompanyCode)
+//       .collection('DepartmentMaster').add({ Name: EmpMaster.Department });
+
+//     const empRef = await db.collection('CompanyMaster').doc(data.CompanyCode)
+//       .collection('EmpMaster').doc(EmpMaster['PSNo']).set(EmpMaster);
+
+//     empRef.writeTime;
+
+//     response.json({ success: 'true', message: 'You Have successfully registered' });
+
+//   } catch (error) {
+
+//     response.status(500).send(error);
+
+//   }
+// });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
